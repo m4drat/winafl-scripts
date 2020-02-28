0f2a60b4f5014c609dc6dfcd43b7106a447fbebe
m4drat <ftri2001@gmail.com>
2020-02-28 10:51:07 +0300

    Fuzz script rewritten to work with config file

--- fuzz.py
+++ fuzz.py
@@ -1,58 +1,45 @@
 import subprocess
 import threading
 import time
+import json
 import sys
 
-TARGET = 0x24620 # 0x6DF0: 3
+def load_config(path):
+    cfg = open(path).read()
+    return json.loads(cfg)
 
-D_RIO_HOME = 'C:\\Users\\madrat\\Desktop\\RE\\DynamoRIO-Windows-7.91.18278-0\\'
-W_AFL_HOME = 'C:\\Users\\madrat\\Desktop\\RE\\winafl\\'
-BASE_DIR   = 'C:\\Users\\madrat\\Desktop\\fuzz\\'
-APP = 'CFF_Explorer_check_patched.exe'
-
-INP_DIR = BASE_DIR + 'testcases\\minset\\'
-OUT_DIR = BASE_DIR + 'out\\'
-
-# debug run:
-# Z:\DynamoRIO-Windows-7.1.0-1\bin32\drrun.exe -c Z:\winafl\bin32\winafl.dll -debug -coverage_module CFF_Explorer.exe -target_module CFF_Explorer.exe -target_offset 0x6df0 -nargs 3 -call_convention thiscall -- CFF_Explorer.exe Z:\fuzz\testcases\pe_small\tinydll.dll
-
-# WORKERS = 4
-
-ARCH = '32' # can be 32 or 64
-
-def spawn_fuzzer(worker : int, is_master : bool):
+def spawn_fuzzer(worker : int, is_master : bool, args : dict):
     cmdline = [
-        W_AFL_HOME + f'bin{ARCH}\\afl-fuzz.exe',
+        args['W_AFL_HOME'] + f'bin{args["ARCH"]}\\afl-fuzz.exe',
         '-D',
-        D_RIO_HOME + f'bin{ARCH}',
+        args['D_RIO_HOME'] + f'bin{args["ARCH"]}\\',
         '-i',
-        '-', # INP_DIR,
+        args['FUZZ']['INP_DIR'],
         '-o',
-        OUT_DIR,
+        args['FUZZ']['OUT_DIR'],
         '-M' if is_master else '-S',
         f'fuzzer_{worker}',
         '-t',
-        '3000',
+        args['FUZZ']['TIMEOUT'],
         '-I',
         '15000+',
         '--',
         '-target_module',
-        APP,
-        '-target_offset',
-        hex(TARGET),
-        '-nargs',
-        '1',
-        '-call_convention',
-        'thiscall',
+        args['FUZZ']['TARGET_MODULE'],
         '-coverage_module',
-        APP,
+        args['FUZZ']['COVERAGE_MODULE'],
+        '-target_offset',
+        hex(int(args['FUZZ']['TARGET_OFFSET'], 16)),
+        '-nargs',
+        args['FUZZ']['NARGS'],
+        '-call_convention',
+        args['FUZZ']['CALL_CONVENTION'],
         '-covtype',
-        'edge',
+        args['FUZZ']['COVTYPE'],
         '-fuzz_iterations',
-        '5000',
+        args['FUZZ']['FUZZ_ITER'],
         '--',
-        BASE_DIR + APP,
-        '@@'
+        ' '.join(args['FUZZ']['TARGET_CMD'])
     ]
 
     print('[+] Fuzzer commad-line: ' + ' '.join(cmdline))
@@ -61,39 +48,23 @@ def spawn_fuzzer(worker : int, is_master : bool):
         try:
             sp = subprocess.Popen(
                 cmdline,
-                # stdout=subprocess.PIPE, 
-                # stderr=subprocess.PIPE,
-                cwd=W_AFL_HOME + f'bin{ARCH}\\'
+                cwd=args['W_AFL_HOME'] + f'bin{args["ARCH"]}\\'
             )
             sp.wait()
         except:
             print(f'[-] fuzzer_{worker} died! Restarting instance!')
 
         cmdline[cmdline.index('-i') + 1] = '-'
-
         print(f'Restarting fuzzer with new cmdline: {" ".join(cmdline)}')
 
         time.sleep(2)
         
 def main():
-    # print(f'[+] Spawning {WORKERS} fuzzers!')
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
         worker = int(sys.argv[1])
-        # for worker in range(0, WORKERS):
-        threading.Timer(0.0, spawn_fuzzer, args=[worker, True if worker == 0 else False]).start()
-
-        if worker == 0:
-            time.sleep(5.0)
-
-        # while threading.active_count() > 1:
-        #     time.sleep(5.0)
-
-        #     try:
-        #         subprocess.check_call(['py', '-3', W_AFL_HOME + 'winafl-whatsup.py', "-s", OUT_DIR])
-        #     except:
-        #         pass
+        threading.Timer(0.0, spawn_fuzzer, args=[worker, True if worker == 0 else False, load_config(sys.argv[2])]).start()
     else:
-        print(f'Usage: {sys.argv[0]} <fuzzer_id>')
+        print(f'Usage: {sys.argv[0]} <fuzzer_id> <config.json>')
 
 if __name__ == "__main__":
     main()
