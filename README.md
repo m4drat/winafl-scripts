0b5051ae08a793d632ffabd77daf896ae98adda6
m4drat <ftri2001@gmail.com>
2020-02-29 11:48:43 +0300

    Support for config file for all scripts

---
+++ README.md
@@ -0,0 +1,1 @@
+# Winafl-scripts
--- crash_analyzer.py
+++ crash_analyzer.py
@@ -1,3 +1,5 @@
+from config_loader import load_config
+
 import subprocess
 import threading
 import argparse
@@ -5,13 +7,7 @@ import pathlib
 import queue
 import sys
 
-APP = 'CFF_Explorer_check_patched.exe'
-BUGID_DIR = 'C:\\Users\\madrat\\Desktop\\RE\\BugId\\'
-BASE_DIR  = 'C:\\Users\\madrat\\Desktop\\fuzz\\'
-
-WORKERS = 8
-
-def run_bugid(tasks : queue.Queue, worker_id : int):
+def run_bugid(tasks : queue.Queue, worker_id : int, args : dict):
     while True:
         params = tasks.get()
         crash_path  = params['crash_path']
@@ -23,10 +19,9 @@ def run_bugid(tasks : queue.Queue, worker_id : int):
         print(f'({worker_id}) Processing: "{crash_path}". ({current}/{total})')
 
         cmdline = [
-            BUGID_DIR + 'BugId.cmd',
-            '--nApplicationMaxRunTimeInSeconds=6',
-            '--isa=x86',
-            BASE_DIR + APP,
+            args['CRASH_ANALYZER']['BUGID_DIR'] + 'BugId.cmd',
+            ' '.join(args['CRASH_ANALYZER']['ADDITIONAL_CMD']),
+            args['CRASH_ANALYZER']['TARGET_APP'],
             '--',
             crash_path
         ]
@@ -46,47 +41,50 @@ def run_bugid(tasks : queue.Queue, worker_id : int):
         tasks.task_done()
 
 def main():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('-c', '--crashes_dir', dest='inp',     help='fuzzers sync dir')
-    # parser.add_argument('-o', '--out_dir',     dest='out',     help='output dir')
-    # parser.add_argument('-d', '--delete',      dest='delete',  help='delete test cases, that didnt lead to crash', default=False)
-    # args = parser.parse_args() 
+    if len(sys.argv) > 1:
+        args = load_config(sys.argv[1])
 
-    crashes_dir = pathlib.Path('C:\\Users\\madrat\\Desktop\\fuzz\\fuzzers_results\\crashes\\')
-    reports_dir = pathlib.Path('C:\\Users\\madrat\\Desktop\\fuzz\\fuzzers_results\\crash_analysis_results\\')
+        if args == None:
+            exit(1)
 
-    if not crashes_dir.exists():
-        print('Crashes directory doesnt exist! Check if file path is correct!')
-        exit(1)
+        crashes_dir = pathlib.Path(args['CRASH_ANALYZER']['INP_DIR'])
+        reports_dir = pathlib.Path(args['CRASH_ANALYZER']['REPORTS_DIR'])
 
-    if not reports_dir.exists():
-        print('Out directory doesnt exist! Creating new one.')
-        reports_dir.mkdir(parents=True, exist_ok=True)
+        if not crashes_dir.exists():
+            print('Crashes directory doesnt exist! Check if file path is correct!')
+            exit(1)
 
-    tasks = queue.Queue()
+        if not reports_dir.exists():
+            print('Out directory doesnt exist! Creating new one.')
+            reports_dir.mkdir(parents=True, exist_ok=True)
 
-    for worker_id in range(WORKERS):
-        worker = threading.Thread(target=run_bugid, args=(tasks, worker_id))
-        worker.setDaemon(True)
-        worker.start()
+        tasks = queue.Queue()
 
-    crash_samples = list(crashes_dir.iterdir())
-    current = 0
-    total   = len(crash_samples)
+        for worker_id in range( args['CRASH_ANALYZER']['WORKERS']):
+            worker = threading.Thread(target=run_bugid, args=(tasks, worker_id, args))
+            worker.setDaemon(True)
+            worker.start()
 
-    for crash_path in crash_samples:
-        tasks.put(
-            {
-                'crash_path'  : crash_path.absolute().as_posix(),
-                'reports_dir' : reports_dir.absolute().as_posix(),
-                'current'     : current,
-                'total'       : total,
-                'delete'      : False
-            }
-        )
-        current += 1
+        crash_samples = list(crashes_dir.iterdir())
+        current = 0
+        total   = len(crash_samples)
 
-    tasks.join()
+        for crash_path in crash_samples:
+            tasks.put(
+                {
+                    'crash_path'  : crash_path.absolute().as_posix(),
+                    'reports_dir' : reports_dir.absolute().as_posix(),
+                    'current'     : current,
+                    'total'       : total,
+                    'delete'      : args['CRASH_ANALYZER']['DELETE_NON_CRASHING']
+                }
+            )
+            current += 1
+
+        tasks.join()
+    else:
+        print(f'Usage: {sys.argv[0]} <config_file>')
+
 
 if __name__ == "__main__":
     main()
